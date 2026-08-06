608185524a66bca9602e6f3a05e0d1eca7ea818c
Aadarsh ojha <aadarshojha46@gmail.com>
2026-08-06 13:47:20 +0530

    Add files via upload

---
+++ associativearr.php
@@ -0,0 +1,17 @@
+<?php
+$students =[
+    "name" => "addy",
+    "age " => 22,
+    "class" => "10th"
+];
+$multi =[
+    "name" => "addddy",
+    "age" => 22
+];
+echo $multi["age"];
+echo $multi["name"];
+
+echo $students["name"];
+echo $students["age"];
+
+?>
