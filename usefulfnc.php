c0ca99814839c50d058233f28a9966374ffab472
Aadarsh ojha <aadarshojha46@gmail.com>
2026-08-08 16:43:36 +0530

    Add files via upload

---
+++ usefulfnc.php
@@ -0,0 +1,39 @@
+<?php
+$arr = [22,44,45,67];
+echo count($arr);
+?>
+<?php
+$arr1 = [10,20];
+array_push($arr1,209);
+print_r($arr1);
+?>
+
+<?php
+$arr4 = [22,44,44,56];
+array_pop($arr4);
+print_r($arr4);
+
+?>
+
+<?php
+$arr = [2,6,7];
+sort($arr);
+print_r($arr)
+?>
+<?php
+$arr = [7,8,9];
+rsort($arr);
+print_r($arr)
+
+?>
+<?php
+$arr = [22,33,56];
+array_push($arr,88);
+print_r($arr);
+?>
+<?php
+$arr =[23,56,43];
+array_pop($arr);
+print_r($arr);
+?>
+
